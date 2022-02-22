b08e8ef72a558123d0a82d861ebb6ba4a440deb3
ElCabronw <lucasfuziyama@gmail.com>
2022-02-22 19:08:29 -0300

    Incluindo chamada ao back end

--- Front/ProEventos-APP/src/app/eventos/eventos.component.ts
+++ Front/ProEventos-APP/src/app/eventos/eventos.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +8,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EventosComponent implements OnInit {
 
-  constructor() { }
+  public eventos: any;
+
+  constructor(private http: HttpClient) { }
 
   ngOnInit() {
+    this.getEventos()
   }
+
+  public getEventos():void{
+    this.http.get('https://localhost:5001/api/eventos').subscribe(
+      response => this.eventos = response,
+      error => console.log(error)
+    )
+  //   this.eventos = [
+  //     {
+  //     Tema: 'Angular',
+  //     Local: 'Belo Horizonte'
+
+  //   },
+  //   {
+  //     Tema: '.net 5',
+  //     Local: 'Contagem'
+
+  //   },
+  //   {
+  //     Tema: 'React',
+  //     Local: 'Betim'
+
+  //   },]
+   }
 
 }
